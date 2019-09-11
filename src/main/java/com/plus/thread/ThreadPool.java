b7e734edf78d62e1c641ce33054c80a3f517956f
pyyyo <a1450650519@163.com>
2019-09-11 21:29:30 +0800

    修改线程池配置

--- src/main/java/com/plus/thread/ThreadPool.java
+++ src/main/java/com/plus/thread/ThreadPool.java
@@ -3,7 +3,7 @@ package com.plus.thread;
 import java.util.concurrent.*;
 
 public class ThreadPool {
-    private ExecutorService executorService;
+    private ThreadPoolExecutor executorService;
     /**
      * Runtime.getRuntime().availableProcessors()方法返回到Java虚拟机的可用的处理器数量，最大线程数需大于等于它，否则报错。
      * @param maxPoolSize 最大线程数
@@ -11,6 +11,7 @@ public class ThreadPool {
     public ThreadPool(int maxPoolSize) {
         executorService = new ThreadPoolExecutor(Runtime.getRuntime().availableProcessors(),
                 maxPoolSize, 120L, TimeUnit.SECONDS, new LinkedBlockingQueue<>());
+        executorService.allowCoreThreadTimeOut(true);
     }
 
     public void execute(Runnable task) {
